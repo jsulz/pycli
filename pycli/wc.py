b6fdab43f06950517e413fc3f560c9dbd1118043
jsulz <j.sulzdorf@gmail.com>
2024-07-05 14:26:33 -0700

    rough draft of wc done

--- pycli/wc.py
+++ pycli/wc.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-# output is always in the order of: bytes\tlines\tcharacters\twords
+# default is always in the order of: lines\tc\tbytes
 
 
 class WC:
@@ -11,16 +11,53 @@ class WC:
         self.n_lines = n_lines
         self.n_chars = n_chars
         self.n_words = n_words
-        self.output = ""
 
     def wc(self):
 
         for file in self.files:
             if self.c_bytes:
-                try:
-                    self.output += str(os.stat(file).st_size)
-                except FileNotFoundError as e:
-                    print(e)
-                except IsADirectoryError as e:
-                    print(e)
-        sys.stdout.write(self.output + "\n")
+                bs = self.count_bytes(file)
+            if self.n_lines or self.n_chars or self.n_words:
+                l, c, w = self.count_more(file)
+
+            output = (
+                    f"{str(l) + "\t" if self.n_lines else ""}"
+                    f"{str(w) + "\t" if self.n_words else ""}"
+                    f"{str(bs) + "\t" if self.c_bytes else ""}"
+                    f"{str(c) + "\t" if self.n_chars else ""}"
+                    f"{file}"
+                )
+            sys.stdout.write(output + "\n")
+
+    def _c_gen(self, reader):
+        b = reader(2**16)
+        while b:
+            yield b
+            b = reader(2**16)
+
+    def count_more(self, file):
+        sum_lines = 0
+        sum_chars = 0
+        sum_words = 0
+        try:
+            with open(file, "rb") as f:
+                c_gen = self._c_gen(f.raw.read)
+                for buffer in c_gen:
+                    sum_lines += buffer.count(b"\n")
+                    b_decoded = buffer.decode("utf-8")
+                    sum_chars += len(b_decoded)
+                    sum_words += len(b_decoded.split())
+        except FileNotFoundError as e:
+            print(e)
+        except IsADirectoryError as e:
+            print(e)
+
+        return sum_lines, sum_chars, sum_words
+
+    def count_bytes(self, file):
+        try:
+            return str(os.stat(file).st_size)
+        except FileNotFoundError as e:
+            print(e)
+        except IsADirectoryError as e:
+            print(e)
